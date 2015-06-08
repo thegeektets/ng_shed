9cf7315d01b1127d01324b574dbab1221c9a1a71
Griffin Muteti <griffin@Tiger.local>
2015-06-08 11:29:58 +0300

    search book functionality

--- app.js
+++ app.js
@@ -354,8 +354,55 @@ function ListBookCtrl($scope, Restangular) {
 	$scope.books = Restangular.all("books").getList().$object;
 }
 
-function AddBookCtrl ($scope, $location, Restangular) {
-   
+function AddBookCtrl ($scope, $location, Restangular,$http) {
+  $scope.addmaunally = false;
+
+$scope.manual = function(){
+  $scope.addmaunally = true;
+
+}
+
+   var pendingTask;
+ 
+$scope.change = function(){
+  if(pendingTask) {
+    clearTimeout(pendingTask);
+
+  }
+  pendingTask = setTimeout(fetch, 800);
+
+  
+
+};
+
+function fetch() {
+  $http.get("https://www.googleapis.com/books/v1/volumes?q=" + 
+     $scope.search + "&tomatoes=true&plot=full")
+   .success(function(response){
+    $scope.details = response;
+
+  $scope.book.title = $scope.details.items['0'].volumeInfo.title;
+  $scope.book.description = $scope.details.items['0'].volumeInfo.description;
+  $scope.book.publisher = $scope.details.items['0'].volumeInfo.publisher;
+  $scope.book.author = $scope.details.items['0'].volumeInfo.authors['0'];
+  $scope.book.dateofpublication = $scope.details.items['0'].volumeInfo.publishedDate;
+  $scope.book.pic = $scope.details.items['0'].volumeInfo.imageLinks.thumbnail;
+  $scope.book.category = $scope.details.items['0'].volumeInfo.categories['0'];
+          
+  $scope.book.librarytype = 'Personal';
+
+
+  });
+
+
+}
+$scope.update = function(movie){
+  $scope.search = movie.Title;
+  $scope.change();
+};
+$scope.select = function(){
+  this.setSelectionRange(0, this.value.length);
+}   
     $scope.book={};
     $scope.teamreferences= [{
            id: 'Branding',
@@ -379,7 +426,7 @@ function AddBookCtrl ($scope, $location, Restangular) {
         });
     }
 }
-function ViewBookCtrl($rootScope,$scope, $location, Restangular, book){
+function ViewBookCtrl($rootScope,$scope, $location, Restangular, book,$http){
   $scope.relatedbooks = Restangular.all("books").getList().$object;
 
 	var original = book;
